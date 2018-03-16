d951f015a11d591787765d5e7257e973f9c82042
Hannan Saddiq <saddiqhb@gmail.com>
2018-03-16 21:49:48 +0000

    Added intial test for IPFS
    If run as node script, will act as a test for node.js IPFS file storage system

---
+++ IPFS.js
@@ -0,0 +1,18 @@
+const IPFS = require('ipfs-mini');
+const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+
+ipfs.add('hello world!', (err, result) => {
+  console.log(err, result);
+});
+
+// result null 'QmTp2hEo8eXRp6wg7jXv1BLCMh5a4F3B7buAUZNZUu772j'
+
+ipfs.cat('QmTp2hEo8eXRp6wg7jXv1BLCMh5a4F3B7buAUZNZUu772j', (err, result) => {
+  console.log(err, result);
+});
+
+// result null 'hello world!'
+
+ipfs.addJSON({ somevalue: 2, name: 'Nick' }, (err, result) => {
+  console.log(err, result);
+});
