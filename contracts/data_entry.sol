3a70252b8016b6c40842cf3fb079fc15cec16a10
Hannan Saddiq <saddiqhb@gmail.com>
2018-03-17 16:59:43 +0000

    updated IPFS function

--- contracts/data_entry.sol
+++ contracts/data_entry.sol
@@ -4,9 +4,8 @@ contract DataInfo{
    mapping(address=>Data[]) private data_points;
    struct Data{
       string dataHash;
-      string ipfsInfo;
    }
-   function uploaddata(string hash, string ipfs) public{
-       datapoints[msg.sender].push(Data(hash,ipfs)); //
+   function uploaddata(string hash) public{
+       datapoints[msg.sender].push(Data(hash)); //
    }
 }
