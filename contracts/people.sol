52717aa6743654f535f8df7279b443e8640bd729
Mackachoo <33980362+Mackachoo@users.noreply.github.com>
2018-03-17 14:30:25 +0000

    ahhh

--- contracts/people.sol
+++ contracts/people.sol
@@ -1,37 +1,7 @@
 pragma solidity ^0.4.17;
 
 
-contract Person {
-
-    function basicInfo (string _name, uint _date, string _gen, string _nat) {
-        require(search());
-        string name = _name;
-        uint dob = _date;
-        string gender = _gen;
-        string nat = _nat;
-    }
-
-    function returnBasicInfo () returns(string name, uint dob, string gender, string nat) {
-        require(search());
-        return (name, dob, gender, nat);
-    }
-
-    function search () public {
-        for (uint i = 0; i < authorized.length; i++) {
-            if (msg.sender == authorized[i]) {
-                return true;
-            }
-            if (msg.sender == admin) {
-                return 45;
-            } else {
-                return false;
-            }
-        }
-    }
-}
-
-
-contract Patient is Person {
+contract Patient {
 
     address admin;
     address[] public authorized;
@@ -41,34 +11,65 @@ contract Patient is Person {
         string desc;
     }
 
+    struct Basic {
+        string name;
+        uint date;
+        string gen;
+        string _nat;
+    }
+
     Record[] public records;
 
-    function addRecord (string storage _ofType, string storage _desc) {
-        require(search());
-        records.push(Record(_type, _desc));
+    function basicInfo (string _name, uint _date, string _gen, string _nat) {
+        require(search() == 61);
+        Basic info = Basic(_name, _date, _gen, _nat);
+    }
+
+    function returnBasicInfo () returns(string name, uint dob, string gender, string nat) {
+        require(search() == 61);
+        return (name, dob, gender, nat);
+    }
+
+    function addRecord (string _ofType, string _desc) {
+        require(search() == 61);
+        records.push(Record(_ofType, _desc));
     }
 
     function addAuthorized (address _auth) {
         require(search() == 45);
-        Authorized.push(_auth);
+        authorized.push(_auth);
     }
 
     function removeAuthorized (address _auth) {
-        for (uint e = 0; e < athorized.length; i++) {
-            if (_auth == authorized[i]) {
-                delete authorized[i];
+        require(search() == 45);
+        for (uint e = 0; e < authorized.length; e++) {
+            if (_auth == authorized[e]) {
+                delete authorized[e];
+            }
+        }
+    }
+
+    function search () public returns(uint ) {
+        for (uint i = 0; i < authorized.length; i++) {
+            if (msg.sender == authorized[i]) {
+                return 61;
+            }
+            if (msg.sender == admin) {
+                return 45;
+            } else {
+                return 12;
             }
         }
     }
 }
 
 
-contract Doctor is Person {
+contract Doctor {
 
     address[] patients;
 
     function getBasicData (address _add) {
-        Patient pt = Patient();
+
     }
 
     function getRecords (address _add) {
