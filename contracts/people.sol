9ba6a48aaa7fb610206335157b28af63cb2dc134
JavantaM <37458120+JavantaM@users.noreply.github.com>
2018-03-17 22:26:51 +0000

    _removed

---
+++ contracts/people.sol
@@ -0,0 +1,73 @@
+pragma solidity ^0.4.17;
+
+
+contract Patient {
+
+    address admin;
+    address[] public authorized;
+
+    struct Record {
+        string ofType;
+        string desc;
+    }
+
+    struct Information {
+        string name;
+        uint date;
+        string gen;
+        string nat;
+    }
+
+    Record[] public records;
+
+    function basicInfo (string _name, uint _date, string _gen, string _nat) {
+        require(search() == 61);
+        Information memory info = Information(_name, _date, _gen, _nat);
+    }
+
+    function addRecord (string _ofType, string _desc) {
+        require(search() == 61);
+        records.push(Record(_ofType, _desc));
+    }
+
+    function addAuthorized (address _auth) {
+        require(search() == 45);
+        authorized.push(_auth);
+    }
+
+    function removeAuthorized (address _auth) {
+        require(search() == 45);
+        for (uint e = 0; e < authorized.length; e++) {
+            if (_auth == authorized[e]) {
+                delete authorized[e];
+            }
+        }
+    }
+
+    function search () public returns(uint ) {
+        for (uint i = 0; i < authorized.length; i++) {
+            if (msg.sender == authorized[i]) {
+                return 61;
+            }
+            if (msg.sender == admin) {
+                return 45;
+            } else {
+                return 12;
+            }
+        }
+    }
+}
+
+
+contract Doctor {
+
+    address[] patients;
+
+    function getBasicData (address _add) {
+
+    }
+
+    function getRecords (address _add) {
+
+    }
+}
